878c2957a241e21ece861106776dde083f2af83e
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 01:17:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(x)
   end
   return "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
